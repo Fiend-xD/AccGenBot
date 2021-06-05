5342b32ccadb86c206bd8021df24b88e72732f86
Fiend-xD <78103261+Fiend-xD@users.noreply.github.com>
2021-06-05 15:14:26 +0530

    Update start.py

--- AccGenBot/plugins/start.py
+++ AccGenBot/plugins/start.py
@@ -5,7 +5,7 @@ from Configs import Config
 
 @AccGen.on(events.NewMessage(pattern="^[!?/]start$"))
 async def start(event):
-    await AccGen.send_message(Config.LOGS_CHAT, f"Bot Started By [{event.sender.first_name}](tg://user?id={event.sender_id}).")
+    await AccGen.send_message(Config.LOGS_CHAT, f"Bot Started By {event.sender.first_name}")
     check = await verify(Config.CHANNEL_US, event, AccGen)
     if check is False:
        await event.reply("**Join my channel to use me:)**", buttons=[
